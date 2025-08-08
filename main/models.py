c611517ca928de86c0f076f28698d2804504b1dc
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-08-08 16:15:00 +0000

    .

--- main/models.py
+++ main/models.py
@@ -42,7 +42,7 @@ class Model:
         raise TimeoutError(f"🟥 Ollama server did not start in time.")
 
     async def warm_up(self):
-        async with aiofiles.open("/main/logs/SD_ERROR.txt", "w") as f:
+        async with aiofiles.open("/logs/SD_ERROR.txt", "w") as f:
             # await self.process = subprocess.Popen(
             #         self.start_command, 
             #         env=self.ollama_env, 
